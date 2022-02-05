45fdc386530e6978f31ba5c98696097f08e42022
Alexander Trakhimenok <alex@trakhimenok.com>
2022-02-05 11:58:47 +0000

    end2end_test_ramsqldb.go - reuse common options

--- end2end/end2end_test.go
+++ end2end/end2end_test.go
@@ -1,9 +1,21 @@
 package end2end
 
-import "testing"
+import (
+	"github.com/strongo/dalgo2sql"
+	"testing"
+)
 
 func TestEndToEnd(t *testing.T) {
+	options := dalgo2sql.Options{
+		Recordsets: map[string]dalgo2sql.Recordset{
+			"DalgoE2E_E2ETest1": {
+				Name:       "E2ETest1",
+				Type:       dalgo2sql.Table,
+				PrimaryKey: []dalgo2sql.Field{{Name: "ID1"}},
+			},
+		},
+	}
 	t.Run("RAMSQLDB", func(t *testing.T) {
-		testEndToEndRAMSQLDB(t)
+		testEndToEndRAMSQLDB(t, options)
 	})
 }
--- end2end/end2end_test_ramsqldb.go
+++ end2end/end2end_test_ramsqldb.go
@@ -7,20 +7,11 @@ import (
 	"testing"
 )
 
-func testEndToEndRAMSQLDB(t *testing.T) {
+func testEndToEndRAMSQLDB(t *testing.T, options dalgo2sql.Options) {
 	db := ramsqldb.OpenTestDb(t)
 	defer func() {
 		_ = db.Close()
 	}()
-	options := dalgo2sql.Options{
-		Recordsets: map[string]dalgo2sql.Recordset{
-			"DalgoE2E_E2ETest1": {
-				Name:       "E2ETest1",
-				Type:       dalgo2sql.Table,
-				PrimaryKey: []dalgo2sql.Field{{Name: "ID1"}},
-			},
-		},
-	}
 	database := dalgo2sql.NewDatabase(db, options)
 	end2end.TestDalgoDB(t, database)
 }
